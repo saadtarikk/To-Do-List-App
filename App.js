1a021e1397e70ef8c4d35d8b1911f5cdc1857b0c
Saad Tariq <saad.tarik@outlook.com>
2021-07-28 21:06:31 +0100

    pushing remaining changes

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, FlatList, ScrollView, Alert } from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem'
+import AddItem from './components/AddItem'
+import {v4 as uuid} from 'uuid';
 
-export default function App() {
+const App = () => {
+
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk' },
+    {id: uuid(), text: 'Juice' },
+    {id: uuid(), text: 'Bread' },
+    {id: uuid(), text: 'Eggs' },
+    {id: uuid(), text: 'Crisps' },
+  ]);
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  }
+
+  const addItem = (text) => {
+
+    if(!text) {
+      Alert.alert('Error', 'Please enter an item', {text: 'Ok'})
+    } else{
+      setItems(prevItems => {
+        return[{id: uuid.toString(), text: text}, ...prevItems]
+      })
+    }  
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}> 
+      <Header title="Shopping List"></Header>
+      <FlatList 
+        data={items} 
+        renderItem={({item}) => 
+        <ListItem item={item} deleteItem={deleteItem} />}
+      />
+      <AddItem addItem={addItem}/>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  flex: 1,
+  backgroundColor: 'black',
+  paddingTop: 50,
   },
+  text: {
+    color: 'white',
+    fontSize: 30,
+  }
 });
+
+export default App;
